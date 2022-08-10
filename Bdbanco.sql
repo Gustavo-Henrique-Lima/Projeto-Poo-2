b974fddf08eb8bc65c4c6e2731abb6a554de4efe
Gustavo Henrique De Lima Nascimento <90515231+Gustavo-Henrique-Lima@users.noreply.github.com>
2022-08-10 15:55:06 -0300

    Update Bdbanco.sql

--- Bdbanco.sql
+++ Bdbanco.sql
@@ -1,31 +1,3 @@
-/*Cliente PJ*/
-select * from clientepj;
-select * from enderecoclientepj;
-select * from contacorrentepj;
-select * from telefonesclientepj;
-select * from contapoupancapj;
-select * from transacaoclientepj;
-delete from contapoupancapj where cnpj_cliente="12345678910000";
-delete from enderecoclientepj where cnpj_cliente="12345678910000";
-delete from telefonesclientepj where cnpj_cliente="12345678910000";
-delete from clientepj where cnpj="12345678910000";
-/*---------------*/
-
-/*Cliente PF*/
-select * from clientepf;
-select * from telefonesclientepf;
-select * from enderecoclientepf;
-select * from contacorrentepf;
-delete from contapoupancapf where saldo=0;
-delete from enderecoclientepf where cidade="Cuiabá";
-delete from telefonesclientepf where telefones="81989668924";
-delete from clientepf where cpf="99999999999";
-select * from contapoupancapf;
-select * from estados;
-select * from transacaoclientepf;
-
-
-
 
 create table contapoupancapj
 (
@@ -53,9 +25,3 @@ create table cidades
     primary key (id),
     foreign key (id_estado) references estados(id)
 );
-select * from estados;
-select * from cidades;
-insert into cidades values (0,"Maceió",2),(0,"Macapá",3),(0,"Manaus",4),(0,"Salvador",5),(0,"Fortaleza",6),(0,"Brasília",7),
-(0,"Vitória",8),(0,"Goiana",9),(0,"Vitória",10),(0,"Cuiabá",11),(0,"Campo Grande",12),(0,"Belo Horizonte",13),(0,"Belém",14),(0,"João Pessoa",15),
-(0,"Curitiba",16),(0,"Recife",17),(0,"Teresina",18),(0,"Rio de Janeiro",19),(0,"Natal",20),(0,"Porto Alegre",21),(0,"Porto Velho",22),(0,"Boa Vista",23),
-(0,"Florianópolis",24),(0,"São Paulo",25),(0,"Aracaju",26),(0,"Palmas",27);
